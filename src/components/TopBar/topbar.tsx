79cff3b2c48cf24874fc8ec6b45f74e96bc81b6e
moise10r <moiserushanika2006@gmail.com>
2022-11-22 21:31:25 +0200

    Add the baseBtn layout component.

--- src/components/TopBar/topbar.tsx
+++ src/components/TopBar/topbar.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import styles from './topbar.module.scss'
+import BaseButton from '../common/baseBtn/BaseBtn';
 export default function Topbar() {
   return (
-    <div className={styles.topBarMainContainer}>Topbar</div>
+    <div className={styles.topBarMainContainer}>
+        <div className={styles.rightContainer}>
+            <div className={styles.folder}>
+                <div className={styles.folderIcon}>0</div>
+                <div className={styles.folderText}>Folder</div>
+            </div>
+        </div>
+        <div className={styles.leftContainer}>
+            <div className={styles.sharedMyNote}>
+                <BaseButton />
+            </div>
+        </div>
+    </div>
   )
 }
