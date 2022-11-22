3a8a05237cd0def7c0e4a0af265ea1bfbd902631
moise10r <moiserushanika2006@gmail.com>
2022-11-22 03:12:55 +0200

    Set up the project structure.

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Home.module.scss'
+export default function Home() {
+  return (
+    <div className={styles.homeMainContainer}>
+      <div className="homeLeftMainContainer">
+        
+      </div>
+      <div className="homeRightMainContainer"></div>
+
+    </div>
+  )
+}
