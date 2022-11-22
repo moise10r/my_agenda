da82fe08185ee7996b188afda6fef1937c28c3fa
moise10r <moiserushanika2006@gmail.com>
2022-11-23 00:43:42 +0200

    Add the agendalist object.

---
+++ src/utils/agenda.ts
@@ -0,0 +1,40 @@
+const agendaList = [
+    {
+        id: '1',
+        title: 'Introduction',
+        description: 'Welcome to the workshop',
+        createdAt: '2020-01-01T00:00:00.000Z'
+    },
+    {
+        id: '2',
+        title: 'Agenda',
+        description: 'What we will cover today',
+        createdAt: '2020-01-01T00:00:00.000Z'
+    },
+    {
+        id: '3',
+        title: 'Demo',
+        description: 'How to build a GraphQL API',
+        createdAt: '2020-01-01T00:00:00.000Z'
+    },
+    {
+        id: '4',
+        title: 'Q&A',
+        description: 'Ask us anything',
+        createdAt: '2020-01-01T00:00:00.000Z'
+    },
+    {
+        id: '5',
+        title: 'Wrap up',
+        description: 'Thank you for coming',
+        createdAt: '2020-01-01T00:00:00.000Z'
+    },
+    {
+        id: '6',
+        title: 'Next steps',
+        description: 'Where to go from here',
+        createdAt: '2020-01-01T00:00:00.000Z'
+    }
+]
+
+export default agendaList
